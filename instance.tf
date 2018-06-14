745ab0fb043790ad326611fe90981c96c62c0f3d
russmalloypers <russell.malloy@gmail.com>
2018-06-14 13:56:11 +0000

    updated file contents

--- instance.tf
+++ instance.tf
@@ -8,16 +8,34 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.mykey.key_name}"
 
+  
   provisioner "file" {
     source = "script.sh"
     destination = "/tmp/script.sh"
-                     }
-    provisioner "remote-exec" {
-      inline = [
-        "chmod +x /tmp/script.sh",
- "sudo /tmp/script.sh"
-        ]
-                       }
+  }
+  provisioner "file" {
+    source = "custom_404.html"
+    destination = "/tmp/custom_404.html"
+  }
+  provisioner "file" {
+    source = "nginx.conf"
+    destination = "/tmp/nginx.conf"
+  }
+  provisioner "remote-exec" {
+    inline = [
+    "chmod +x /tmp/script.sh",
+    "chmod 755 /tmp/nginx.conf",
+    "sudo /tmp/script.sh"
+    ]
+  }
+  
+  
+  provisioner "file" {
+    source = "helloworld.py"
+    destination = "/tmp/helloworld.py"
+  }
+  
+  
     connection {
  user = "${var.INSTANCE_USERNAME}"
         private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo systemctl enable nginx
+sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf
+sudo mv /tmp/custom_404.html /usr/share/nginx/html/custom_404.html
+sudo systemctl restart nginx
